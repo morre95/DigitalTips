775553c9dcbc0c7f0755874cef648c079cf8d646
josefswadijohansson <swadijosef@gmail.com>
2025-02-03 12:49:45 +0100

    Added the necessary buttons

--- app/Settings.tsx
+++ app/Settings.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Alert, View, Text, StyleSheet, Button } from 'react-native';
 
 export default function Settings() {
+
+  const alertUser = (input : string) => {
+    Alert.alert(input);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Settings Screen</Text>
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => {alertUser("credits");}} title="Credits" color="#1abc9c"/>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => {alertUser("permissions");}} title="Manage Permissions" color="#1abc9c"/>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => {alertUser("privacy");}} title="Privacy Policy" color="#1abc9c"/>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={() => {alertUser("terms");}} title="Terms & Conditions" color="#1abc9c"/>
+      </View>
     </View>
   );
 }
@@ -13,6 +29,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    gap: 10,
+    marginTop: 32,
+  },
+  buttonContainer: {
+    width: 200,
+    height: 50,
   },
 });
