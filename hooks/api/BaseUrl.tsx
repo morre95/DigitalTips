c44178d9ec6ff8e22ae02fd8691d37daef29e85e
Erik Morén <erik.moren78@gmail.com>
2025-02-19 14:38:53 +0100

    changed BaseUrl to morencv.se

--- hooks/api/BaseUrl.tsx
+++ hooks/api/BaseUrl.tsx
@@ -2,7 +2,8 @@
 
 enum BaseUrl {
     local = 'http://10.0.2.2/myProjects/slimPhp4Test_Slask/',
-    remote = 'http://tipsdigitial.mygamesonline.org/',
+    //remote = 'http://tipsdigitial.mygamesonline.org/',
+    remote = 'https://digitaltips.morencv.se/',
 }
 
 /*enum BaseUrl {
