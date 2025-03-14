984633fb7bd52fd0a6c96d6a6a3d0db912430db9
Erik Morén <erik.moren78@gmail.com>
2025-03-14 09:03:41 +0100

    Do it again do it right

--- components/create_route/CityComponent.tsx
+++ components/create_route/CityComponent.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Modal, TextInput, Button } from 'react-native';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import {StyleSheet, Text, View, Modal, Button, Pressable} from 'react-native';
+import Checkbox from 'expo-checkbox';
+
+interface City {
+    city: string;
+    selected: boolean;
+}
 
 interface Props {
     citys: string[]
@@ -8,71 +13,58 @@ interface Props {
 }
 
 export default function CityComponent({ citys, onChange }: Props) {
-    const [cityState, setCityState] = useState<string[]>(citys);
+    const [cityState, setCityState] = useState<City[]>([]);
+    const [editCityState, setEditCityState] = useState<City[]>([]);
     const [modalVisible, setModalVisible] = useState(false);
-    const [inputValue, setInputValue] = useState('');
-    const [editIndex, setEditIndex] = useState(-1);
-
-    const handelDelete = (cityToEdit: string) => {
-        setCityState(preCitys => preCitys.filter((city) => city !== cityToEdit));
-    }
-
-    const handelEdit = (index: number) => {
-        setModalVisible(true);
-        setInputValue(cityState[index]);
-        setEditIndex(index)
-    }
 
     const handleSave = () => {
-        if (editIndex >= 0) {
-            setCityState(preCitys => preCitys.map((oldValue, index) => {
-                if (index === editIndex)
-                    return inputValue
-                else
-                    return oldValue
-            }));
-        } else {
-            setCityState([...cityState, inputValue])
-        }
+        setCityState(editCityState)
+        setModalVisible(false)
+    }
 
-        setModalVisible(false);
-        setInputValue('');
-        setEditIndex(-1)
-    };
+    const handelCancel = () => {
+        setEditCityState(cityState)
+        setModalVisible(false)
+    }
 
-    const handleAdd = () => {
-        setEditIndex(-1)
-        setModalVisible(true);
+    const handlePressed = () => {
+        setModalVisible(true)
+    }
+
+    const handleValueChange = (index: number) => {
+        setEditCityState(prevCitys => prevCitys.map((city, i) => {
+            if (i === index) {
+                return {...city, selected: !prevCitys[i].selected}
+            }
+            return city;
+        }))
     }
 
     useEffect(() => {
-        onChange(cityState)
-    }, [cityState]);
+        const mappedCities = citys.filter(city => city.length > 0).map(city => ({
+            city,
+            selected: true,
+        }));
+        setCityState(mappedCities)
+        setEditCityState(mappedCities)
+    }, [])
+
+    useEffect(() => {
+        onChange(cityState.filter(city => city.selected).map(city => city.city))
+    }, [cityState])
 
     return (
         <View style={styles.container}>
-            {cityState.map((item: string, index: number) => (
-                <View key={index} style={styles.item}>
-                    <Text>
-                        {item}
+            <Pressable
+                onPress={handlePressed}
+                style={styles.items}
+            >
+                {cityState.filter(city => city.selected).map((item: City, index: number) => (
+                    <Text key={index} style={styles.item}>
+                        {item.city}
                     </Text>
-                    <AntDesign.Button
-                        name="delete"
-                        size={24}
-                        color="black"
-                        backgroundColor="rgba(52, 52, 52, 0)"
-                        onPress={() => handelDelete(item)}
-                    />
-                    <AntDesign.Button
-                        name="edit"
-                        size={24}
-                        color="black"
-                        backgroundColor="rgba(52, 52, 52, 0)"
-                        onPress={() => handelEdit(index)}
-                    />
-                </View>
-            ))}
-            <Button title="Add" onPress={handleAdd} />
+                ))}
+            </Pressable>
             <Modal
                 animationType="slide"
                 transparent={true}
@@ -81,15 +73,21 @@ export default function CityComponent({ citys, onChange }: Props) {
             >
                 <View style={styles.modalView}>
                     <View style={styles.modalWrapper}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Change me"
-                            value={inputValue}
-                            onChangeText={setInputValue}
-                        />
+                        <Text>Selected Citys:</Text>
+                        {editCityState.map((item: City, index: number) => (
+                            <View key={index} style={styles.modalEdit}>
+                                <Checkbox
+                                    value={item.selected}
+                                    onValueChange={() => handleValueChange(index)}
+                                />
+                                <Text key={index} style={styles.item}>
+                                    {item.city}
+                                </Text>
+                            </View>
+                        ))}
                         <View style={styles.modalButtons}>
                             <Button title="Save" onPress={handleSave} />
-                            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+                            <Button title="Cancel" onPress={handelCancel} />
                         </View>
                     </View>
                 </View>
@@ -105,10 +103,17 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         padding: 5,
     },
-    item: {
+    items: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        width: '100%',
         padding: 10,
         borderColor: '#ccc',
         borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: '#e8e8e8',
+    },
+    item: {
         margin: 5,
     },
     modalView: {
@@ -118,24 +123,17 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     modalWrapper: {
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 12,
         width: '100%',
         backgroundColor: '#fff',
     },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        width: '80%',
-        marginBottom: 20,
-        paddingHorizontal: 10,
-        backgroundColor: 'white',
+    modalEdit: {
+        flexDirection: 'row',
+        marginVertical: 5,
     },
     modalButtons: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        width: '80%',
+        width: '100%',
     },
 });
