827bc97349a3080f6785c75ec44ed37852e0cf21
Erik Morén <erik.moren78@gmail.com>
2025-05-10 16:25:28 +0200

    validating url

--- components/maps/GameUrlComponent.tsx
+++ components/maps/GameUrlComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Modal } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, Modal, TouchableOpacity, ToastAndroid } from 'react-native';
 import * as Linking from 'expo-linking';
 import * as Clipboard from 'expo-clipboard';
 import {getRouteInfo, RouteInfo} from "@/functions/api/Get";
@@ -7,6 +7,7 @@ import {useToken} from "@/components/login/LoginContext";
 
 import { Link } from 'expo-router';
 import Spacer from "@/components/Spacer";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 
 interface GameUrlComponentProps {
@@ -20,7 +21,7 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
     const {token} = useToken();
     const [routeInfo, setRouteInfo] = useState<RouteInfo | null>(null);
 
-    // Start app with URL
+    // Start the app with URL
     useEffect(() => {
         let subscription: any
         (async () => {
@@ -43,9 +44,14 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
     };
 
     const handleUrl = async (url: string) => {
-        const { queryParams } = Linking.parse(url);
+        if (!isValidUrl(url)) {
+            showToastMsg('The url has the wrong format');
+            showToastMsg('The right format: something://something?id=[route id]');
+        }
+
+        const { queryParams} = Linking.parse(url);
         if (queryParams?.id) {
-            const id = Number(queryParams.id)
+            const id = Number(queryParams.id);
             setRouteId(id);
             const result = await getRouteInfo(id, token as string);
             if (!result.error) {
@@ -53,6 +59,8 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
             } else {
                 setRouteInfo(null);
             }
+        } else {
+            showToastMsg(`Can not find any game with: '${url}'`);
         }
     }
 
@@ -60,6 +68,20 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
         close();
         setRouteId(null);
     }
+
+    const showToastMsg = (message: string) => {
+        ToastAndroid.showWithGravity(
+            message,
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER,
+        );
+    };
+
+    const isValidUrl = (urlString: string): boolean => {
+        const urlRegex = /^[A-Za-z0-9.-]+:\/\/[A-Za-z0-9.-]+[?]id=\d+$/
+        return urlRegex.test(urlString);
+    }
+
 
     // appToTheApp://gameToPlay?id=5
 
@@ -69,7 +91,9 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
             transparent={true}
             visible={visible}
             onRequestClose={handleClose}>
-            <View style={styles.centeredView}>
+            <TouchableOpacity
+                onPress={handleClose}
+                style={styles.centeredView}>
                 <View style={styles.container}>
                     {routeId ? (
                         <View>
@@ -83,7 +107,7 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
                                         params: { routeId: routeId },
                                     }}
                                     >
-                                        Start game
+                                        <MaterialCommunityIcons name="gamepad-square-outline" size={60} color="black" />
                                     </Link>
                                 </>
                                 :
@@ -94,18 +118,26 @@ export default function GameUrlComponent({visible, close}: GameUrlComponentProps
                         </View>
                     ) : (
                         <View>
-                            <TextInput
-                                style={styles.input}
-                                value={url}
-                                onChangeText={setUrl}
-                                placeholder={'URL'}
-                                onSubmitEditing={() => handleUrl(url)}
-                            />
-                            <Button title="View copied text" onPress={fetchCopiedText} />
+                            <Text>Example: example://startGame?id=4</Text>
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                                    <TextInput
+                                        style={styles.input}
+                                        value={url}
+                                        onChangeText={setUrl}
+                                        placeholder={'URL'}
+                                        onSubmitEditing={() => handleUrl(url)}
+                                    />
+                                <TouchableOpacity onPress={() => handleUrl(url)}>
+                                    <Text style={styles.linkText}>Check</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <TouchableOpacity style={styles.button} onPress={fetchCopiedText}>
+                                <Text style={styles.buttonText}>Paste URL</Text>
+                            </TouchableOpacity>
                         </View>
                     )}
                 </View>
-            </View>
+            </TouchableOpacity>
         </Modal>
     );
 }
@@ -132,9 +164,30 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
     input: {
+        flex: 1,
         height: 40,
         margin: 12,
         borderWidth: 1,
         padding: 10,
+        alignItems: 'stretch'
     },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderWidth: 1,
+        backgroundColor: '#0569FF',
+        borderColor: '#0569FF',
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    linkText: {
+        marginTop: 20,
+        color: '#0569FF'
+    }
 });
